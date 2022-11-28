1c5bf5c19c9cf108fd3823f149039d43721a4a13
Subhasmita Sahu <subhasmita.sahu@gyansys.com>
2022-11-28 11:35:03 +0530

    first commit

---
+++ SingleTone/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonConsoleApp
+{
+
+    public class Singleton
+    {
+        private static Singleton _instance;     //Static property with
+
+
+        private Singleton() 
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) 
+                
+                _instance = new Singleton();
+
+
+                return _instance;
+            }
+        }
+
+        public void DoSingletonOperation() { 
+            
+            Console.WriteLine("singleton operation");      
+            }  
+    }    
+    
+  class Program  
+  {        
+        static void Main(string[] args)     
+        {
+
+
+        Singleton singletonOld = Singleton.Instance;
+
+
+        Singleton singletonNew = Singleton.Instance;
+
+
+         Console.WriteLine(singletonOld == singletonNew);  
+            
+         Console.ReadLine();
+
+        }
+  }
+}
+
+
+
